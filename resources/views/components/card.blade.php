6fa115db9fca9517408f4c1e9ef5a516cec8b56a
shafikul <shafikul.18288@gmail.com>
2024-10-08 07:59:42 +0600

    ###

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,38 +1,23 @@
-<div class="flex flex-col overflow-hidden rounded-lg shadow-lg transition-transform duration-300 hover:scale-105 dark:bg-gray-800 dark:text-gray-300">
-    <div class="flex-shrink-0">
-        <img class="h-48 w-full object-cover"
-            src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1679&amp;q=80"
-            alt="">
-    </div>
-    <div class="flex flex-1 flex-col justify-between bg-white p-6 dark:bg-gray-900">
-        <div class="flex-1">
-            <p class="text-sm font-medium text-indigo-600 dark:text-indigo-400">
-                <a href="#" class="hover:underline">Article</a>
-            </p>
-            <a href="#" class="mt-2 block">
-                <p class="text-xl font-semibold text-gray-900 dark:text-gray-100 lg:text-2xl">Boost your conversion rate</p>
-                <p class="mt-3 text-base text-gray-500 dark:text-gray-400 lg:text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.</p>
+@props([
+    'blockClass' => '',
+    'contentBlockClass' => '',
+])
+<div {{ $attributes }}>
+    <div class="{{ $blockClass }}">
+        <a href="">
+            <img class="max-w-full w-full mx-auto" src="{{ asset('img/dummy/img6.jpg') }}" alt="alt title">
+        </a>
+        <div class="{{ $contentBlockClass }}">
+            <h3 class="text-lg font-bold leading-tight mb-2">
+                <a href="#">5 Tips to Save Money Booking Your Next Hotel Room</a>
+            </h3>
+            <p class="hidden md:block text-gray-600 leading-tight mb-1">This is a wider card with supporting
+                text below as a natural lead-in to additional content.</p>
+            <a class="text-gray-500" href="#">
+                <span class="inline-block h-3 border-l-2 border-red-600 mr-2">
+                </span>
+                Europe
             </a>
-        </div>
-        <div class="mt-6 flex items-center">
-            <div class="flex-shrink-0">
-                <a href="#">
-                    <span class="sr-only">Roel Aufderehar</span>
-                    <img class="h-10 w-10 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80"
-                        alt="">
-                </a>
-            </div>
-            <div class="ml-3">
-                <p class="text-sm font-medium text-gray-900 dark:text-gray-100">
-                    <a href="#" class="hover:underline">Roel Aufderehar</a>
-                </p>
-                <div class="flex space-x-1 text-sm text-gray-500 dark:text-gray-400">
-                    <time datetime="2020-03-16">Mar 16, 2020</time>
-                    <span aria-hidden="true">·</span>
-                    <span>6 min read</span>
-                </div>
-            </div>
         </div>
     </div>
 </div>
