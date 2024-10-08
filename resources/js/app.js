00f752f2bb75a98da4c67b6488b3874fc09d6c50
shafikul <shafikul.18288@gmail.com>
2024-10-08 19:51:55 +0600

    slider added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,16 @@
 import './bootstrap';
+import '@splidejs/splide/css';
 
 import Alpine from 'alpinejs';
+import Splide from '@splidejs/splide';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#post-carousel', {
+        type: 'loop',
+        perPage: 3,
+    }).mount();
+});
