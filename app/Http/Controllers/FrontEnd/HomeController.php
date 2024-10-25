701559ab33dd5a4c7d8728d567877ef268dc322d
shafik <shaifk108@gmail.com>
2024-10-25 12:22:43 +0600

    home hero section modify homecontroller and home component

--- app/Http/Controllers/FrontEnd/HomeController.php
+++ app/Http/Controllers/FrontEnd/HomeController.php
@@ -2,18 +2,50 @@
 
 namespace App\Http\Controllers\FrontEnd;
 
+use App\Models\Category;
 use App\Models\post\Post;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
-    public function home(){
+    public function home()
+    {
         return view('home');
     }
 
-    public function heroSection(){
-        $allPost = Post::all();
-        return view('home', compact('allPost'));
+    public function heroSection()
+    {
+        $heroSection = Category::with(['posts' => function ($query) {
+            $query->latest()->take(1);
+        }])->limit(5)->get();
+
+        $returnVal = $heroSection->map(function ($item) {
+            return [
+                'category' => [
+                    'name' => $item->name,
+                    'slug' => $item->slug
+                ],
+                'post' => [
+                    'title' => $this->cutString($item->posts[0]->title, 5),
+                    'slug' => $item->posts[0]->slug,
+                    'featured_image' => $item->posts[0]->featured_image,
+                    'alt_name' => $item->posts[0]->alt_name,
+                    'published_at' => $item->posts[0]->published_at,
+                    'content' => $this->cutString($item->posts[0]->content, 5)
+                ]
+            ];
+        });
+        return response()->json(['data' => $returnVal]);
+    }
+
+    private function cutString($string, $count){
+
+        $content = strip_tags($string);
+        $content = trim($content);
+
+        $words = explode(' ', $content);
+        $countWord = count($words);
+        return ($countWord > $count) ? implode(' ', array_slice($words, 0, $count)) . '...' :  $contents = implode(' ', $words); 
     }
 }
--- resources/views/Frontend/heroSection.blade.php
+++ resources/views/Frontend/heroSection.blade.php
@@ -3,119 +3,89 @@
         <!-- big grid 1 -->
         <div class="flex flex-row flex-wrap">
             <!--Start left cover-->
-            <div class="flex-shrink max-w-full w-full lg:w-1/2 pb-1 lg:pb-0 lg:pr-1">
-                <div class="relative hover-img max-h-98 overflow-hidden">
-                    <a href="#">
-                        <img class="max-w-full w-full mx-auto h-auto" src="{{ asset('img/dummy/img1.jpg') }}""
-                            alt="Image description">
-                    </a>
-                    <div class="absolute px-5 pt-8 pb-5 bottom-0 w-full bg-gradient-cover">
-                        <a href="#">
-                            <h2 class="text-3xl font-bold capitalize text-white mb-3">Amazon Shoppers Are Ditching
-                                Designer Belts
-                                for This Best-Selling</h2>
-                        </a>
-                        <p class="text-gray-100 hidden sm:inline-block">This is a wider card with supporting text below
-                            as a
-                            natural lead-in to additional content. This very helpfull for generate default content..</p>
-                        <div class="pt-2">
-                            <div class="text-gray-100">
-                                <div class="inline-block h-3 border-l-2 border-red-600 mr-2"></div>Europe
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div class="flex-shrink max-w-full w-full lg:w-1/2 pb-1 lg:pb-0 lg:pr-1" id="heroSectionFirst">
+               
             </div>
 
             <!--Start box news-->
             <div class="flex-shrink max-w-full w-full lg:w-1/2">
-                <div class="box-one flex flex-row flex-wrap">
-                    <article class="flex-shrink max-w-full w-full sm:w-1/2">
-                        <div class="relative hover-img max-h-48 overflow-hidden">
-                            <a href="#">
-                                <img class="max-w-full w-full mx-auto h-auto" src="{{ asset('img/dummy/img2.jpg') }}"
-                                    alt="Image
-                                    description">
-                            </a>
-                            <div class="absolute px-4 pt-7 pb-4 bottom-0 w-full bg-gradient-cover">
-                                <a href="#">
-                                    <h2 class="text-lg font-bold capitalize leading-tight text-white mb-1">News
-                                        magazines are becoming
-                                        obsolete, replaced by gadgets</h2>
-                                </a>
-                                <div class="pt-1">
-                                    <div class="text-gray-100">
-                                        <div class="inline-block h-3 border-l-2 border-red-600 mr-2"></div>Techno
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </article>
-                    <article class="flex-shrink max-w-full w-full sm:w-1/2">
-                        <div class="relative hover-img max-h-48 overflow-hidden">
-                            <a href="#">
-                                <img class="max-w-full w-full mx-auto h-auto" src="{{ asset('img/dummy/img3.jpg') }}"
-                                    alt="Image
-                                    description">
-                            </a>
-                            <div class="absolute px-4 pt-7 pb-4 bottom-0 w-full bg-gradient-cover">
-                                <a href="#">
-                                    <h2 class="text-lg font-bold capitalize leading-tight text-white mb-1">Minimalist
-                                        designs are
-                                        starting to be popular with the next generation</h2>
-                                </a>
-                                <div class="pt-1">
-                                    <div class="text-gray-100">
-                                        <div class="inline-block h-3 border-l-2 border-red-600 mr-2"></div>Architecture
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </article>
-                    <article class="flex-shrink max-w-full w-full sm:w-1/2">
-                        <div class="relative hover-img max-h-48 overflow-hidden">
-                            <a href="#">
-                                <img class="max-w-full w-full mx-auto h-auto" src="{{ asset('img/dummy/img4.jpg') }}"
-                                    alt="Image
-                                    description">
-                            </a>
-                            <div class="absolute px-4 pt-7 pb-4 bottom-0 w-full bg-gradient-cover">
-                                <a href="#">
-                                    <h2 class="text-lg font-bold capitalize leading-tight text-white mb-1">Tips for
-                                        decorating the
-                                        interior of the living room</h2>
-                                </a>
-                                <div class="pt-1">
-                                    <div class="text-gray-100">
-                                        <div class="inline-block h-3 border-l-2 border-red-600 mr-2"></div>Interior
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </article>
-                    <article class="flex-shrink max-w-full w-full sm:w-1/2">
-                        <div class="relative hover-img max-h-48 overflow-hidden">
-                            <a href="#">
-                                <img class="max-w-full w-full mx-auto h-auto" src="{{ asset('img/dummy/img5.jpg') }}"
-                                    alt="Image
-                                    description">
-                            </a>
-                            <div class="absolute px-4 pt-7 pb-4 bottom-0 w-full bg-gradient-cover">
-                                <a href="#">
-                                    <h2 class="text-lg font-bold capitalize leading-tight text-white mb-1">Online taxi
-                                        users are
-                                        increasing drastically ahead of the new year</h2>
-                                </a>
-                                <div class="pt-1">
-                                    <div class="text-gray-100">
-                                        <div class="inline-block h-3 border-l-2 border-red-600 mr-2"></div>Lifestyle
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </article>
+                <div class="box-one flex flex-row flex-wrap allContent" id="allContent">
+                    <h2 class="font-bold text-4xl text-indigo-600 lodingText"></h2>
+                    <!--All Content-->
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+<script>
+    const lodingText = document.querySelector('.lodingText');
+    const allContent = document.getElementById('allContent')
+    const heroSectionFirst = document.getElementById('heroSectionFirst')
+    let heroFristContent = null;
+
+    lodingText.innerHTML = 'Loding ... ';
+    const singlePostRoute = "{{ route('singlePost', ':slug') }}";
+
+    axios.get("{{ route('heroSection') }}")
+    .then(response =>{
+        if (response.status == 200) {
+            lodingText.innerHTML = ' ';
+
+            response.data.data.forEach((element, index) => {
+                if (index === 0) {
+                    heroFristContent = element;
+                    return; 
+                }
+                const postUrl = singlePostRoute.replace(':slug', element.post.slug);
+
+                allContent.innerHTML += `
+                 <article class="flex-shrink max-w-full w-full sm:w-1/2">
+                        <div class="relative hover-img max-h-48 overflow-hidden">
+                            <a href="${postUrl}">
+                                <img class="max-w-full w-full mx-auto h-auto" src="{{ asset('storage/${element.post.featured_image}') }}"
+                                    alt="${element.post.alt_name}">
+                            </a>
+                            <div class="absolute px-4 pt-7 pb-4 bottom-0 w-full bg-gradient-cover">
+                                <a href="${postUrl}">
+                                    <h2 class="text-lg font-bold capitalize leading-tight text-white mb-1">${element.post.title}</h2>
+                                </a>
+                                <div class="pt-1">
+                                    <div class="text-gray-100">
+                                        <div class="inline-block h-3 border-l-2 border-red-600 mr-2"></div>${element.category.name}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </article>
+                `
+            });
+
+            const FirstPostUrl = singlePostRoute.replace(':slug', heroFristContent.post.slug);
+            heroSectionFirst.innerHTML = `
+             <div class="relative hover-img max-h-98 overflow-hidden">
+                    <a href="${FirstPostUrl}">
+                        <img class="max-w-full w-full mx-auto h-auto" src="{{ asset('storage/${heroFristContent.post.featured_image}') }}""
+                            alt="${heroFristContent.post.alt_name}">
+                    </a>
+                    <div class="absolute px-5 pt-8 pb-5 bottom-0 w-full bg-gradient-cover">
+                        <a href="${FirstPostUrl}">
+                            <h2 class="text-3xl font-bold capitalize text-white mb-3">${heroFristContent.post.title}</h2>
+                        </a>
+                        <p class="text-gray-100 hidden sm:inline-block">${heroFristContent.post.title}</p>
+                        <div class="pt-2">
+                            <div class="text-gray-100">
+                                <div class="inline-block h-3 border-l-2 border-red-600 mr-2"></div>${heroFristContent.category.name}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `
+        }
+    }).catch(err => {
+        lodingText.innerHTML = 'Error API ... ';
+        console.log(err);
+    })
+
+</script>
