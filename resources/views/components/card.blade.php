9f2c1f225258f4f19c758a39aa359344631efca5
shafik <shaifk108@gmail.com>
2024-10-30 12:29:44 +0600

    no error fix cardcomponent src url

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,44 +1,36 @@
-@props([
-    'blockClass' => '',
-    'contentBlockClass' => '',
-    'img' => '',
-    'alt' => '',
-    'heading' => '',
-    'content' => '',
-    'category' => '',
-    'categorySlug' => '',
-    'author' => '',
-    'postTime' => '',
-    'slug' => '',
-])
+    @props([
+        'blockClass' => '',
+        'contentBlockClass' => '',
+        'img' => '',
+        'alt' => '',
+        'heading' => '',
+        'content' => '',
+        'category' => '',
+        'categorySlug' => '',
+        'author' => '',
+        'postTime' => '',
+        'slug' => '',
+    ])
 
-<div {{ $attributes }}>
-    <div class="{{ $blockClass }}">
-        <a href="{{ url('blog/'. $slug) }}">
-            <img class="max-w-full w-full mx-auto" 
-            @php
-                if (!$img) {
-                    echo "src='https://cdn.pixabay.com/photo/2019/12/24/09/54/the-gargoyle-4716390_1280.jpg'";
-                } elseif (preg_match('/^(https?:\/\/|www\.)/', $img)) {
-                    echo "src='" . $img . "'";
-                } else {
-                    echo "src='" . asset('storage/' . $img) . "'";
-                }
-            @endphp
-        
-            alt="{{ $alt ? $alt : 'no caption' }}" 
-            loding="lazy">
-        </a>
-        <div class="{{ $contentBlockClass }}">
-            <h3 class="text-lg font-bold leading-tight mb-2">
-                <a href="{{ url('blog/'. $slug) }}">{{ $heading }}</a>
-            </h3>
-            <p class="hidden md:block text-gray-600 leading-tight mb-1">{{ $content }}</p>
-            <a class="text-gray-500" href="{{ url('categories/'. $categorySlug) }}">
-                <span class="inline-block h-3 border-l-2 border-red-600 mr-2">
-                </span>
-                {{ $category }}
+    <div {{ $attributes }}>
+        <div class="{{ $blockClass }}">
+            <a href="{{ url('blog/' . $slug) }}">
+                <img class="max-w-full w-full mx-auto" src="{{ $img }}" alt="{{ $alt ? $alt : 'no caption' }}"
+                    loading="lazy">
+
             </a>
+            <img src="{{ asset('') }}" alt="">
+            <div class="{{ $contentBlockClass }}">
+                <h3 class="text-lg font-bold leading-tight mb-2">
+                    <a href="{{ url('blog/' . $slug) }}">{{ $heading }}</a>
+                </h3>
+                <p class="hidden md:block text-gray-600 leading-tight mb-1">{{ $content }}</p>
+                <a class="text-gray-500" href="{{ url('categories/' . $categorySlug) }}">
+                    <span class="inline-block h-3 border-l-2 border-red-600 mr-2">
+                    </span>
+                    {{ $category }}
+                </a>
+            </div>
         </div>
     </div>
-</div>
+    {{-- src="{{ Str::startsWith($img, ['http://', 'https://', 'www.']) ? $img : asset('storage/' . ltrim($img, '/')) }}" --}}
